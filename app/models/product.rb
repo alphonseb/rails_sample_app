4ea87cb80480ab257617993d7192784d6136aad4
Alphonse Bouy <alphonse.bouy@gmail.com>
2020-02-03 11:54:09 +0000

    pagination

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
     validates :title, presence: true
     validates :description, presence: true
     validates :price, presence: true, numericality: { greater_than_or_equal_to: 0}
+    paginates_per 5
 end
